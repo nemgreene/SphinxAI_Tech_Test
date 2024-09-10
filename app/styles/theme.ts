437f70b53ddc2d501f7c824bf4113f4117452376
Nem <vincent.r.greene@gmail.com>
2024-09-10 09:55:52 +0100

    added MUI for theming

---
+++ app/styles/theme.ts
@@ -0,0 +1,11 @@
+"use client";
+import { Roboto } from "next/font/google";
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+export default theme;
